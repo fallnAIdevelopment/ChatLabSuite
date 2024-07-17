731415cb3ba867f890ca387b3059ef0b226895b7
fallnAIdevelopment <falln87ai@gmail.com>
2024-07-17 02:26:37 -0700

    Update app.config.ts
    Changed Brand

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -8,23 +8,23 @@
  */
 export const Brand = {
   Title: {
-    Base: 'big-AGI',
-    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
+    Base: 'ChatLABS',
+    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'ChatLABS',
   },
   Meta: {
-    Description: 'Launch big-AGI to unlock the full potential of AI, with precise control over your data and models. Voice interface, AI personas, advanced features, and fun UX.',
-    SiteName: 'big-AGI | Precision AI for You',
+    Description: 'Launch ChatLABS to unlock the full potential of AI, with precise control over your data and models. Voice interface, AI personas, advanced features, and fun UX.',
+    SiteName: 'ChatLABSuite |  AI, your way',
     ThemeColor: '#32383E',
-    TwitterSite: '@enricoros',
+    //TwitterSite: '@enricoros',
   },
   URIs: {
-    Home: 'https://big-agi.com',
-    // App: 'https://get.big-agi.com',
-    CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
-    OpenRepo: 'https://github.com/enricoros/big-agi',
-    OpenProject: 'https://github.com/users/enricoros/projects/4',
-    SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
+    Home: 'https://chatlabs.rf.gd',
+    App: 'https://app.chatlabs.rf.gd',
+    CardImage: 'https://chatlabs.rf.gd/img/logo.jpg',
+    // OpenRepo: 'https://github.com/enricoros/big-agi',
+    // OpenProject: 'https://github.com/users/enricoros/projects/4',
+    // SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
     // Twitter: 'https://www.twitter.com/enricoros',
-    PrivacyPolicy: 'https://big-agi.com/privacy',
+    PrivacyPolicy: 'https://fallnai.rf.gd/chatlabs/privacy',
   },
 } as const;
